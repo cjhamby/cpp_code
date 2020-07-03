c29d7795c709801b945f7e3d1e9d59b5abc80381
chris <64818884+cjhamby@users.noreply.github.com>
2020-07-03 17:09:18 -0400

    Create i2c_mod.hpp

---
+++ i2c_mod.hpp
@@ -0,0 +1,17 @@
+// i2c modular functions 
+
+#ifndef I2C_MOD
+#define I2C_MOD
+
+#include "macros.hpp"
+#include "data.hpp"
+
+
+namespace i2c
+{
+    extern void init_i2c(int a);
+    extern void foo();
+    extern void bar();
+}
+
+#endif
